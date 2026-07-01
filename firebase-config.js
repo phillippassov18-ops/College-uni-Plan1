3dfc424b59e608e8ad01dc77f645fdb28a14fb2a
phillippassov18-ops <phillip.passov18@gmail.com>
2026-06-30 20:53:02 -0700

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyC6tLvd0SarH7nhPeKTIfq-sXE8ckEyObs",
+  authDomain: "college-uni-plan.firebaseapp.com",
+  projectId: "college-uni-plan",
+  storageBucket: "college-uni-plan.firebasestorage.app",
+  messagingSenderId: "625825950744",
+  appId: "1:625825950744:web:9b2a1a1af3d58942a1a76d",
+  measurementId: "G-3MHLZB4XN4"
+};
